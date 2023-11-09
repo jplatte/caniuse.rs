acd0709feb2a8c63ee5f67e6514274133ebb16d7
Jonas Platte <jplatte+git@posteo.de>
2023-11-09 09:07:32 +0100

    Simplify conditional CSS class

--- src/components/index.rs
+++ src/components/index.rs
@@ -1,7 +1,7 @@
 use xilem_html::{
     elements::{div, nav},
     interfaces::Element as _,
-    OneOf2, OneOf3, ViewSequence,
+    OneOf3, ViewSequence,
 };
 
 use super::{feature_entry, feature_entry::ShowVersion};
@@ -16,20 +16,13 @@ pub(crate) fn index(
     route: &ListRoute,
     search_scores: &mut [(u16, f64)],
 ) -> impl ViewSequence<AppState> {
-    let stable_link_b = route_link(AppRoute::List(ListRoute::Stable), "Stable");
-    let recently_stabilized_link_b =
-        route_link(AppRoute::List(ListRoute::RecentlyStabilized), "Recently Stabilized");
-    let unstable_link_b = route_link(AppRoute::List(ListRoute::Unstable), "Unstable");
-
-    let stable_link;
-    let recently_stabilized_link;
-    let unstable_link;
+    let mut stable_link_class = None;
+    let mut recently_stabilized_link_class = None;
+    let mut unstable_link_class = None;
 
     let contents = match route {
         ListRoute::Stable => {
-            stable_link = OneOf2::B(stable_link_b.attr("class", "active"));
-            recently_stabilized_link = OneOf2::A(recently_stabilized_link_b);
-            unstable_link = OneOf2::A(unstable_link_b);
+            stable_link_class = Some("active");
             OneOf3::A(
                 div(FEATURES
                     .iter()
@@ -40,10 +33,7 @@ pub(crate) fn index(
             )
         }
         ListRoute::RecentlyStabilized => {
-            stable_link = OneOf2::A(stable_link_b);
-            recently_stabilized_link =
-                OneOf2::B(recently_stabilized_link_b.attr("class", "active"));
-            unstable_link = OneOf2::A(unstable_link_b);
+            recently_stabilized_link_class = Some("active");
             OneOf3::A(
                 div(FEATURES
                     .iter()
@@ -55,9 +45,7 @@ pub(crate) fn index(
             )
         }
         ListRoute::Unstable => {
-            stable_link = OneOf2::A(stable_link_b);
-            recently_stabilized_link = OneOf2::A(recently_stabilized_link_b);
-            unstable_link = OneOf2::B(unstable_link_b.attr("class", "active"));
+            unstable_link_class = Some("active");
             OneOf3::A(
                 div(FEATURES
                     .iter()
@@ -67,48 +55,45 @@ pub(crate) fn index(
                 .attr("class", "feature-list"),
             )
         }
-        ListRoute::SearchResults { input } => {
-            stable_link = OneOf2::A(stable_link_b);
-            recently_stabilized_link = OneOf2::A(recently_stabilized_link_b);
-            unstable_link = OneOf2::B(unstable_link_b.attr("class", "active"));
-
-            match extract_search_terms(input) {
-                Ok(search_terms) => {
-                    let results = run_search(&search_terms, search_scores);
-                    if results.is_empty() {
-                        OneOf3::C(div("Nothing found, sorry.").attr("class", "box muted"))
-                    } else {
-                        OneOf3::B(
-                            div(results
-                                .iter()
-                                .map(|f| feature_entry(f, ShowVersion::Yes))
-                                .collect::<Vec<_>>())
-                            .attr("class", "feature-list"),
-                        )
-                    }
-                }
-                Err(InvalidSearchQuery) => {
-                    OneOf3::C(div("Invalid search terms.").attr("class", "box muted"))
+        ListRoute::SearchResults { input } => match extract_search_terms(input) {
+            Ok(search_terms) => {
+                let results = run_search(&search_terms, search_scores);
+                if results.is_empty() {
+                    OneOf3::C(div("Nothing found, sorry.").attr("class", "box muted"))
+                } else {
+                    OneOf3::B(
+                        div(results
+                            .iter()
+                            .map(|f| feature_entry(f, ShowVersion::Yes))
+                            .collect::<Vec<_>>())
+                        .attr("class", "feature-list"),
+                    )
                 }
             }
-        }
+            Err(InvalidSearchQuery) => {
+                OneOf3::C(div("Invalid search terms.").attr("class", "box muted"))
+            }
+        },
     };
 
     (
         nav(div((
-            stable_link
+            route_link(AppRoute::List(ListRoute::Stable), "Stable")
+                .attr("class", stable_link_class)
                 .on_click(|state: &mut AppState, evt| {
                     state.route = AppRoute::List(ListRoute::Stable);
                     evt.prevent_default();
                 })
                 .passive(false),
-            recently_stabilized_link
+            route_link(AppRoute::List(ListRoute::RecentlyStabilized), "Recently Stabilized")
+                .attr("class", recently_stabilized_link_class)
                 .on_click(|state: &mut AppState, evt| {
                     state.route = AppRoute::List(ListRoute::RecentlyStabilized);
                     evt.prevent_default();
                 })
                 .passive(false),
-            unstable_link
+            route_link(AppRoute::List(ListRoute::Unstable), "Unstable")
+                .attr("class", unstable_link_class)
                 .on_click(|state: &mut AppState, evt| {
                     state.route = AppRoute::List(ListRoute::Unstable);
                     evt.prevent_default();
